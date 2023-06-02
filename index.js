fe8f5a7d0915accaa753be42407329670a0edec1
NirobiaM <mckinneynirobia@gmail.com>
2023-06-02 14:47:11 -0400

    first commit

--- index.js
+++ index.js
@@ -3,20 +3,77 @@
 // let a=7;
 // console.log(a);
 
-let names~["jim","james","paul","tyrone"];
-console.log(names);
+// let names~["jim","james","paul","tyrone"];
+// console.log(names);
 
-let seed1-"carrots";
-console.log(names);
+// let seed1-"carrots";
+// console.log(names);
 
-let x = 28;
-console.log(x);
-// I would like x to equal 28 for some random reason
+// let x = 28;
+// console.log(x);
+// // I would like x to equal 28 for some random reason
 
-let x=12;
-let a=78;
+// let x=12;
+// let a=78;
 
-if(x===12 && a ==78) {
-    let sum=x+a;
-    console.log(sum)}
 
+// let age= 17;
+// if (age > 18) {
+//     //do this
+//     console.log("access granted");
+// }
+// else{
+//     //do this
+//     console.log("acess denied");
+// }
+
+
+// let wood =30 
+// if (wood >40) {
+//     //we have enough for a treehouse
+//     console.log(" BUILD A TREEHOUSE" );
+// }
+// else{
+//     console.log("no treehouse");
+// }
+
+// 
+
+// let num = "nini";
+
+// if(num > 0) {
+//   if (num > 100) {
+//     console.log("num is positive and greater than 100");
+//   } else  {
+//     console.log("num is positive but less than or equal to 100");
+//   }
+// } else {
+//   console.log("num is negative");
+// }
+//  else {
+//     console.log ("not a number");
+// }
+
+
+
+let studentgrade = 75;
+
+if (studentgrade >= 80)
+{
+  console.log("You did a good job");
+
+} else if (studentgrade >= 70) 
+{
+  console.log("Keep trying"
+  );
+} else if (studentgrade >= 60)
+ {
+  console.log("ehhhh");
+
+} else if (studentgrade >= 55)
+ {
+  console.log("Not too good");
+
+} else {
+  console.log("Bad Grade. But you'll get 'em next time!");
+}
